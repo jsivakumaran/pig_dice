3d6b559663d1555b1a74c4b55d9594cd01104fd1
Clem Freeman and Jay Sivakumaran <nayagan@gmail.com>
2015-02-18 11:03:47 -0800

    Add currentScore functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 var Pig = {
   player1: 0,
   player2: 0,
+  currentScore: 0,
   rollDice: function(){
-    return Math.floor(Math.random() * 6) + 1
+    var roll = Math.floor(Math.random() * 6) + 1;
+    if (roll === 1) {
+      this.currentScore = 0;
+    }
+    else {
+      this.currentScore += roll;
+    }
+    return roll;
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,21 @@ describe("Pig", function(){
     var test = Object.create(Pig);
     expect((test.rollDice() <= 6) && (test.rollDice() >= 1)).to.eql(true);
   });
+  it("sets current score to zero if roll is 1", function(){
+    var game = Object.create(Pig);
+    do {
+      var roll = game.rollDice();
+    } while (roll !== 1);
+    expect(game.currentScore).to.eql(0);
+  });
+  it("adds to the currentScore as long as roll is not 1", function(){
+    var game = Object.create(Pig);
+    do {
+      var roll = game.rollDice();
+      if(roll!==1){
+        break;
+      }
+    } while (true);
+    expect(game.currentScore).to.eql(roll);
+  });
 });

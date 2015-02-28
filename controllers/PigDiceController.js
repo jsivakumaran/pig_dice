e84359554be12b05a360d47fe942630bf893a217
jsivakumaran <jsivakumaran@outlook.com>
2015-02-28 13:06:39 -0800

    Switch to angular functions ng-click

--- controllers/PigDiceController.js
+++ controllers/PigDiceController.js
@@ -2,9 +2,9 @@ PigDice.controller('PigDiceCtrl', ['$scope', '$state', 'PigFactory',
 function PigDiceCtrl($scope, $state, PigFactory){
 
   $scope.PigFactory = PigFactory;
+
   $scope.rollDice = function(){
-    debugger;
-    var roll = PigFactory.rollDice();
+    $scope.roll = PigFactory.rollDice();
   }
 
 }]);
